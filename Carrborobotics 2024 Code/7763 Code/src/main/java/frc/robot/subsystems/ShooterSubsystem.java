99d09a5136ea3d9109f0a43bac0824440063b86e
Jason <jmartzloff@gmail.com>
2024-01-29 19:42:24 -0500

    updated buttons for shooter and intake

--- Carrborobotics 2024 Code/7763 Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ Carrborobotics 2024 Code/7763 Code/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -11,15 +11,31 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ShooterSubsystem extends SubsystemBase {
-  /** Creates a new ShooterSubsystem. */
-  CANSparkMax ShooterRight = new CANSparkMax(Constants.ShooterConstants.kShooterRightId, MotorType.kBrushless);
-  CANSparkMax ShooterLeft = new CANSparkMax(Constants.ShooterConstants.kShooterLeftId, MotorType.kBrushless);
-  CANSparkMax intake1 = new CANSparkMax(Constants.ShooterConstants.kintake1Id, MotorType.kBrushless);
-  CANSparkMax intake2 = new CANSparkMax(Constants.ShooterConstants.kintake2Id, MotorType.kBrushless);
-  public ShooterSubsystem() {
-   
+    /** Creates a new ShooterSubsystem. */
+    CANSparkMax ShooterRight = new CANSparkMax(Constants.ShooterConstants.kShooterRightId, MotorType.kBrushless);
+    CANSparkMax ShooterLeft = new CANSparkMax(Constants.ShooterConstants.kShooterLeftId, MotorType.kBrushless);
+    CANSparkMax intake1 = new CANSparkMax(Constants.ShooterConstants.kintake1Id, MotorType.kBrushless);
+    CANSparkMax intake2 = new CANSparkMax(Constants.ShooterConstants.kintake2Id, MotorType.kBrushless);
+    
+    public ShooterSubsystem() {
 
-  }
+    }
+
+    public void shooterFlip(){
+        // Turn shooter on/off
+        double sr = (ShooterRight.get() != 0) ? 0 : 1;
+        double sl = (ShooterLeft.get() != 0) ? 0 : 1;;
+        ShooterRight.set(sr);
+        ShooterLeft.set(sl);
+    }
+
+    public void intakeFlip(){
+        // Turn intake on/off
+        double i1 = (intake1.get() != 0) ? 0 : 1;
+        double i2 = (intake2.get() != 0) ? 0 : 1;;
+        intake1.set(i1);
+        intake2.set(i2);
+    }
 
     public void shooterON(){
         // ArmLeft.setNeutralMode(NeutralMode.Brake);
@@ -27,24 +43,19 @@ public class ShooterSubsystem extends SubsystemBase {
         ShooterLeft.set(50);
     }
     public void intakeON(){
-     intake1.set(50);
-     intake2.set(-50);
+        intake1.set(50);
+        intake2.set(-50);
     }
-    
   
     public void intakeOFF(){
-     intake1.set(0);
-     intake2.set(0);
+        intake1.set(0);
+        intake2.set(0);
     }
-    
 
-  
-
-      public void shooterOFF() {
+    public void shooterOFF() {
         // ArmLeft.setNeutralMode(NeutralMode.Brake);
         ShooterRight.set(0);
         ShooterLeft.set(0);
-    
       }
 
   @Override
